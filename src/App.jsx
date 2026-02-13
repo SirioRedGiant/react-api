3d9cd646bdde8ec6ca3d0875f374dfc1101d21d0
SirioRedGiant <Oiris1992@gmail.com>
2026-02-13 17:11:58 +0100

    Milestone 2-3 => mostrati dati nella card per ogni attrice e implementata della grafica

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { act, useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
@@ -24,14 +24,29 @@ export default function App() {
       <div className="row g-4">
         {actresses.map((actress) => (
           <div key={actress.id} className="col-12 col-md-6 col-lg-4">
-            <div className="card h-100 shadow-sm" style={{ width: "18rem" }}>
+            <div className="card h-100 shadow-sm">
               <img
                 src={actress.image}
                 className="card-img-top"
                 alt={actress.name}
+                style={{ height: "350px", objectFit: "cover" }}
               />
               <div className="card-body">
+                <h3 className="card-title fw-bold">
+                  {actress.name}
+                  <span className="badge text-bg-secondary">
+                    {actress.nationality}
+                  </span>
+                </h3>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Nata nell'anno: {actress.birth_year}
+                </h6>
                 <p className="card-text">{actress.biography}</p>
+                <div className="mt-auto">
+                  <span className="badge bg-warning text-dark">
+                    {actress.awards}
+                  </span>
+                </div>
               </div>
             </div>
           </div>
